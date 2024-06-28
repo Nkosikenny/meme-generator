bf2f76585350e6bd48c2204e2023124dd82d3578
nkosikenny <117735539+Nkosikenny@users.noreply.github.com>
2024-06-28 11:25:59 +0000

    added style and components files

---
+++ meme-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Header from './Components/Header.jsx'
+import Meme from './Components/Meme.jsx'
+
+
+function App() {
+  
+
+  return (
+    <>
+     <Header/>
+     <Meme/>
+    </>
+  )
+}
+
+export default App
---
+++ meme-project/src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import headerImg from "../assets/troll-face.png"
+
+export default function Header() {
+    return (
+        <>
+        <header className="header">
+            <img 
+                src={headerImg} 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+        </>
+    )
+}
